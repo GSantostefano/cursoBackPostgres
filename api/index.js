9aeab106d49b0550514e8064459da2d89e97ee09
GSantostefano <gabrielsantostefano@gmail.com>
2024-09-12 12:28:26 -0300

    1
    1

--- api/index.js
+++ api/index.js
@@ -8,7 +8,8 @@ const port = process.env.PORT || 3000;
 
 app.use(express.json());
 
-const whitelist = ['http://localhost:8080', 'https://myapp.co'];
+const whitelist = ['http://localhost:8080', 'https://myapp.co', 'https://backpostgres.vercel.app'];
+
 const options = {
   origin: (origin, callback) => {
     if (whitelist.includes(origin) || !origin) {
