5d711891bc8a8691887637aa7ac1d32bb9b8d03e
GSantostefano <gabrielsantostefano@gmail.com>
2024-09-12 12:38:44 -0300

    2
    2

--- config/config.js
+++ config/config.js
@@ -2,11 +2,12 @@ require('dotenv').config();
 
 const config = {
   env: process.env.NODE_ENV || 'dev',
-  port: process.envPORT || 3000,
+  port: process.env.PORT || 3000, // Aquí debes corregir el error
   dbUser: process.env.DB_USER,
   dbPassword: process.env.DB_PASSWORD,
   dbHost: process.env.DB_HOST,
   dbName: process.env.DB_NAME,
   dbPort: process.env.DB_PORT,
 };
+
 module.exports = { config };
